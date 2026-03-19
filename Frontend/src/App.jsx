457cf401b0165ad1eaa85c5503ff1e313a59ee82
Sahil Singh <sahilsingh3501@gmail.com>
2026-03-20 03:46:32 +0900

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -102,7 +102,7 @@ export default function App() {
 
             {/* ── Patient Registration ──────────────────────────── */}
             <Route path="/registration/:typedata" element={<Registration />} />
-
+           <Route path="/registration" element={<Registration />} />
             <Route
               path="/registration/:typedata/:id"
               element={<Registration />}
